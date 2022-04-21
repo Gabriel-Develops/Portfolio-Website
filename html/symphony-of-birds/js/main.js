9708ea214e6543501e604dd0b54c024309a516ba
alexrr1997 <86134290+alexrr1997@users.noreply.github.com>
2022-04-20 19:19:36 -0700

    add functionality to symphony project, add pictures for birds and ability to remove them

--- html/symphony-of-birds/js/main.js
+++ html/symphony-of-birds/js/main.js
@@ -8,7 +8,11 @@ function getFetch(){
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
-        addBird(data.recordings[0].file)
+        let bird = addBird(data.recordings[0].file)  // Audio Source
+        if (bird === -1)
+            return
+            
+        addPicofBird(bird, data.recordings[0].en) // Bird Name in English
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -19,15 +23,70 @@ let birdSymphony = []
 
 function addBird(birdSound) {
     // Check to see if bird has already been added
-    if (birdSymphony.indexOf(birdSound) !== -1)
-        return console.error('Bird being added is already in Bird Symphony')
+    if (birdSymphony.indexOf(birdSound) !== -1) {
+        console.error('Bird being added is already in Bird Symphony')
+        return -1
+    }
 
     birdSymphony.push(birdSound)
 
     // Add bird audio element to symphony of birds
-    let bird = document.createElement('audio')
-    bird.setAttribute('autoplay', '')
-    bird.setAttribute('loop', '')
-    bird.src = birdSound
-    document.querySelector('#bird-sounds').appendChild(bird)
+    let birdNoise = document.createElement('audio')
+    birdNoise.setAttribute('autoplay', '')
+    birdNoise.setAttribute('loop', '')
+    birdNoise.src = birdSound
+
+    let bird = document.createElement('div')
+    bird.appendChild(birdNoise)
+
+    document.querySelector('#birds').appendChild(bird)
+    return bird
 }
+
+function addPicofBird(bird, name) {
+    let url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://imsea.herokuapp.com/api/1?q=${name}`
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let title = document.createElement('span')
+        title.innerText = name
+        bird.appendChild(title)
+
+        bird.style.backgroundImage = `url('${data.results[0]}')`
+        bird.classList.add('image')
+
+        bird.addEventListener('click', function() {
+            let position = birdSymphony.indexOf(this.children[0].attributes[2].value)
+            birdSymphony.splice(position, 1)
+
+            this.remove()
+        })
+    })
+    .catch((error) => {
+    console.error('Error:', error);
+    })
+}
+
+
+// fetch(url)
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data)
+
+//     document.querySelector('section').innerHTML = ''
+
+//     let uniqData = Array.from(new Set(data.results))
+//     console.log('unique', new Set(data.results))
+
+//     uniqData.forEach(element => {
+//         let image = document.createElement('img')
+//         image.src = element
+//         image.setAttribute('class', 'masonry-item')
+//         document.querySelector('section').appendChild(image)
+//     })
+// })
+// .catch((error) => {
+// console.error('Error:', error);
+// })

eddce18dbd9033af01d11d8b08e210aa500cf8a1
Gabriel Rodriguez <garodri97@gmail.com>
2022-05-21 22:20:42 -0700

    Soundboard: modal added with customizabily features for removing instructions and customizing colors

--- html/soundboard/js/main.js
+++ html/soundboard/js/main.js
@@ -50,3 +50,79 @@ function printSoundsFromStorage() {
     })
 }
 printSoundsFromStorage()
+
+// KEYBOARD SHORTCUTS
+
+function addShortcuts() {
+    const shortcutArr = Array.from({length: 9}, (_, index) => index)
+
+    for (let shortcut of shortcutArr) {
+        document.addEventListener('keyup', (event) => {
+            const key = event.key
+            if (key != shortcut)
+                return
+
+            const sounds = Array.from(document.querySelectorAll('.sound-effect'))
+            sounds[shortcut - 1].children[1].dispatchEvent(new KeyboardEvent('click'))
+        })
+    }
+}
+addShortcuts()
+
+
+// MODAL
+
+document.querySelector('#modal-container button').addEventListener('click', showModal)
+
+document.querySelector('#modal button').addEventListener('click', hideModal)
+document.querySelector('#modal-shadow').addEventListener('click', hideModal)
+
+function showModal() {
+    document.querySelector('#modal').style.display = 'inline-block'
+    document.querySelector('#modal-shadow').style.display = 'inline-block'
+}
+
+function hideModal() {
+    document.querySelector('#modal').style.display = 'none'
+    document.querySelector('#modal-shadow').style.display = 'none'
+}
+
+// Customizability from User
+// INSTRUCTIONS VISIBILITY TOGGLE
+document.querySelector('#instructionsCheck').addEventListener('click', toggleInstructions)
+function toggleInstructions() {
+    let instructions = document.querySelector('#instructions')
+    let instructionsCheckBox = document.querySelector('#instructionsCheck')
+
+    instructionsCheckBox.checked ? instructions.style.display = 'none' : instructions.style.display = 'inline-block'
+}
+toggleInstructions()
+
+// VIDEO SEARCH VISIBILITY TOGGLE
+document.querySelector('#videoSearchCheck').addEventListener('click', toggleVideoSearch)
+function toggleVideoSearch() {
+    let videoSearch = document.querySelector('#videoSearch')
+    let videoSearchCheckBox = document.querySelector('#videoSearchCheck')
+
+    videoSearchCheckBox.checked ? videoSearch.style.display = 'none' : videoSearch.style.display = 'block'
+}
+toggleVideoSearch()
+
+// BACKGROUND COLOR CUSTOMIZE
+document.querySelector('#applyColor').addEventListener('click', changeColor)
+
+function changeColor() {
+    let backgroundColorLeft = document.querySelector('#background-color-left').value
+    let backgroundColorRight = document.querySelector('#background-color-right').value
+    let soundEffectColorLeft = document.querySelector('#sound-effect-color-left').value
+    let soundEffectColorRight = document.querySelector('#sound-effect-color-right').value
+
+    let body = document.querySelector('body')
+    let soundEffects = document.querySelectorAll('.sound-effect')
+
+    body.style.backgroundImage = `linear-gradient(160deg, ${backgroundColorLeft} 0%, ${backgroundColorRight} 100%)`
+    soundEffects.forEach(soundEffect => soundEffect.style.backgroundImage = `linear-gradient(28deg, ${soundEffectColorLeft} 0%, ${soundEffectColorRight} 100%)`)
+
+    console.log(backgroundColorLeft, backgroundColorRight, soundEffectColorLeft, soundEffectColorRight)
+}
+changeColor()
